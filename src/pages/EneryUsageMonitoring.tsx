e675789b379a91c603d36477a84743155992687e
haeinkang <haein.kang7@gmail.com>
2024-10-21 16:32:18 +0900

    환경변수 변수명 버그 수정: REACT_APP_ 접두사 설정

--- src/pages/EneryUsageMonitoring.tsx
+++ src/pages/EneryUsageMonitoring.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getGas } from '../services'
 
 function EneryUsageMonitoring(props: {}) {
 
+  useEffect(() => {
+    getGasUsage()
+  }, [])
+
   const getGasUsage = async () => {
     try {
       const res = await getGas({
-        serviceKey: 'voTG9o2PZYGCXMPiqf8ntRz+Qwh4i6WrBpmnRy73Aw+hVwSsoOuUHvt7DUvcVpNMpbCJtbi4O1EukuIDLGfGqA==', 
         pageNo: 1, // 페이지번호
         numOfRows: 10, // 한 페이지 결과 수
-        // rlvtYr: '', // 해당 년 
-        // lclgvNm: '', // 지자체명
         returnType: 'json' // 데이터 타입        
       })
   
-      console.log(res);
     } catch (error) {
       console.error(error);
     }
   }
 
-
-  getGasUsage()
 
   return (
     <div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,25 +2,24 @@ import axios from 'axios';
 
 // Axios 인스턴스 생성
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // 기본 API URL 설정
-  timeout: 5000, // 타임아웃 시간 설정 (ms)
+  baseURL: process.env.REACT_APP_BASE_URL, // 기본 API URL 설정
+  timeout: 3000, // 타임아웃 시간 설정 (ms)
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-interface Params {
-  serviceKey: string;
-  pageNo: number;
-  numOfRows: number;
-  rlvtYr?: string;
-  lclgvNm?: string;
-  returnType: 'json' | 'xml';
-}
 
-export const getGas = async (params: Params) => {
+
+export const getGas = async (params: any) => {
   try {
-    const response = await api.get('/getGas', { params });
+    const response = await api.get('getGas', {
+      params: {
+        serviceKey: process.env.REACT_APP_SERVICE_KEY,  
+        returnType: 'json', // 데이터 타입  
+        ...params,
+      } 
+    });
     return response.data;
 
   } catch (error) {
