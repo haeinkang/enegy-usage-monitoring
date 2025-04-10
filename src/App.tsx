50bd4af42fbec7f7c4581da5f7099ee4d141b982
hae._n <haein.kang7@gmail.com>
2025-04-10 19:09:38 +0900

    css body, root 스타일링 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-const Layout = lazy(() => import("./Layout"));
-const EneryUsageMonitoring = lazy(() => import("./pages/EneryUsageMonitoring"));
+import Layout from "./Layout";
+// const EneryUsageMonitoring = lazy(() => import("./pages/EneryUsageMonitoring"));
+const EneryUsageMonitoring = lazy(
+  () =>
+    new Promise<typeof import("./pages/EneryUsageMonitoring")>((resolve) =>
+      setTimeout(() => resolve(import("./pages/EneryUsageMonitoring")), 1000)
+    )
+);
 const NotFound = lazy(() => import("./pages/NotFound"));
-
 function App() {
   return (
-    <Suspense fallback={<div>로딩중...</div>}>
+    <Suspense
+      fallback={
+        <div>
+          <span className="relative flex size-3">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span className="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+        </div>
+      }
+    >
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<EneryUsageMonitoring />} />
